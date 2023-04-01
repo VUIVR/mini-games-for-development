12f1d7e50ad6e04c0ef0d371c4aec2788a200ae8
VUIVR <asostrovskiy@yandex.ru>
2023-04-01 15:19:17 +0400

    start project

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <el-button type="primary">Стартуем</el-button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    title: 'Mini-games for development',
+    modules: [
+        '@element-plus/nuxt',
+        '@pinia/nuxt'
+    ]
+})
