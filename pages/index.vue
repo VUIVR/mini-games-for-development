e452c43509f4a7e244000ef3d54c42777b1b4e45
VUIVR <asostrovskiy@yandex.ru>
2023-04-04 16:35:30 +0400

    Revert "start dev counting game"
    This reverts commit 4f53c47ae006165a23e87402f88ad3fa3496967b.

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,24 @@
 </template>
 
 <script>
+import rusLang from 'element-plus/dist/locale/ru.min' //TODO перенести локализацию в layout
 export default {
   data() {
     return {
+      value1: ''
     }
-  }
+  },
+  computed: {
+    rusLang() { return rusLang }
+  },
 }
+
+
 </script>
+
+<style>
+
+</style>
+
 
 
